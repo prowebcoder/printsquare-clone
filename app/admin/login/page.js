01896392f4d596734dee600744f0c6c8df631f18
prowebcoder <rahul@prowebcoder.com>
2025-11-10 21:18:20 +0530

    Shop changes

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -133,7 +133,7 @@ export default function Login() {
 
           <div className="text-center">
             <p className="text-xs text-gray-500">
-              Default credentials: admin@printsquare.com / admin123
+              Default credentials: admin@printsquare.com / neha@1234
             </p>
             <button
               type="button"
