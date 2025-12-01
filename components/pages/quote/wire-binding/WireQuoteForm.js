ec59c64d40124461181eae86d638536fe29609dd
neha0730 <nehabhardwaz.gtl@gmail.com>
2025-12-01 17:14:37 +0530

    checking

--- components/pages/quote/wire-binding/WireQuoteForm.js
+++ components/pages/quote/wire-binding/WireQuoteForm.js
@@ -500,7 +500,6 @@ const WireQuoteForm = () => {
     <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8 font-sans">
       <div className="max-w-7xl mx-auto">
         
-       
 
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">
